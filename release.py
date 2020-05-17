ba536265c9c7745935b4bc8dfd8a84c4c0a2831e
bjoluc <mail@bjoluc.de>
2020-05-17 13:13:10 +0200

    ci(Release): Add GH_TOKEN authorization to release script

--- release.py
+++ release.py
@@ -3,9 +3,11 @@ Script to optionally do an automated release using commitizen and poetry.
 
 Environment variables:
 * POETRY_PYPI_TOKEN_PYPI
+* GH_TOKEN
 """
 
 import argparse
+import os
 import subprocess
 from typing import List
 
@@ -29,10 +31,11 @@ print("Getting current version...")
 initial_version = get_current_version()
 print("Current version: " + initial_version)
 
+
 bump_args = [
     "--yes",
-    "--changelog",
     "--check-consistency",
+    "--changelog",
 ]
 
 if IS_DRY_RUN:
@@ -42,21 +45,38 @@ if initial_version.startswith("0."):
     print("Current major version is 0. Bumping to 1 and creating initial release.")
     bump_args += ["--increment", "MAJOR"]
 
+    if not IS_DRY_RUN:
+        print("Creating CHANGELOG.md...")
+        subprocess.run(["touch", "CHANGELOG.md"])
+        subprocess.run(["git", "add", "CHANGELOG.md"])
 
 print("\nRunning cz bump...")
 subprocess.run(["cz", "bump"] + bump_args)
-
 
 new_version = get_current_version()
 
 if new_version == initial_version:
     print("Version not changed.")
-else:
-    print("\nVersion changed to " + new_version)
+    exit(0)
 
-    if not IS_DRY_RUN:
-        print("\nRunning git push...")
-        subprocess.run(["git", "push", "--tags"])
+print("\nVersion changed to " + new_version)
 
-        print("Publishing to pypi...")
-        subprocess.run(["poetry", "publish", "--build"])
+if IS_DRY_RUN:
+    exit(0)
+
+print("\nPushing to git repository...")
+
+origin = "origin"
+if "TRAVIS" in os.environ:
+    if "GH_TOKEN" not in os.environ:
+        print("GH_TOKEN environment variable not set.")
+        exit(1)
+
+    origin = "https://{}@github.com/{}.git".format(
+        os.environ["GH_TOKEN"], os.environ["TRAVIS_REPO_SLUG"]
+    )
+
+subprocess.run(["git", "push", origin, "HEAD:master", "--follow-tags", "--tags"])
+
+print("\nPublishing to pypi...")
+subprocess.run(["poetry", "publish", "--build"])

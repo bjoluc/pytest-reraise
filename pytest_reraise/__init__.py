620f1a0232e5a855908755948106526a6b4694a7
bjoluc <mail@bjoluc.de>
2021-06-03 22:23:39 +0200

    fix: Do not rewrite assertions in `pytest_reraise.reraise`
    Motivation: `pytest_reraise.reraise` does not contain any assertions
    that need to be rewritten. Prior to this, in some test environments, the
    call to `pytest.register_assert_rewrite()` would be made twice (for
    whatever reason) and trigger a pytest warning.

--- pytest_reraise/__init__.py
+++ pytest_reraise/__init__.py
@@ -1,10 +1,6 @@
 __version__ = "1.0.3"
 
-import pytest
-
 from pytest_reraise import reraise
 from pytest_reraise.reraise import Reraise
 
 __all__ = ["reraise", "Reraise", "__version__"]
-
-pytest.register_assert_rewrite("pytest_reraise.reraise")
